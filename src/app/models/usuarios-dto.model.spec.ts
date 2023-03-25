1005bad9e94b7e921960ff590349b6eb00777a23
alvaroatencio <alvaroatenciop95@gmail.com>
2023-03-25 14:19:31 -0300

    agregado login, falta testear

---
+++ src/app/models/usuarios-dto.model.spec.ts
@@ -0,0 +1,7 @@
+import { UsuariosDTO } from '../models/usuarios-dto.model';
+
+describe('UsuariosDTO', () => {
+  it('should create an instance', () => {
+    expect(new UsuariosDTO()).toBeTruthy();
+  });
+});
